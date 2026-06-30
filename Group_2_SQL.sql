e5b3e41e9f9559ce04bda8563e524a0023a41436
AbbasN97 <Abbashydernaqvi@gmail.com>
2026-06-30 17:36:04 +0500

    Clarify SQL script execution behavior in comment
    Updated comment to clarify execution behavior across MySQL versions.

--- Group_2_SQL.sql
+++ Group_2_SQL.sql
@@ -1,4 +1,4 @@
--- Please make sure to run this file with execution of SQL script continues after failed statement
+-- Please make sure to run this file with execution of SQL script continues after failed statement due differences in mySQL versions
 -- Total transaction volume processed by Venmo this year
 SELECT SUM(amount) AS total_transaction_volume
 FROM Transaction
